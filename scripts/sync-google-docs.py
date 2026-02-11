71a3dc54b2b7cd6e118d03fc0aad6903c25be785
Sullivan Steele <sullivanrsteele@gmail.com>
2026-02-11 15:25:47 -0500

    Update sync-google-docs.py

--- scripts/sync-google-docs.py
+++ scripts/sync-google-docs.py
@@ -13,6 +13,7 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaIoBaseDownload
 from io import BytesIO
+import zipfile
 
 # Setup
 FOLDER_ID = os.getenv('DRIVE_FOLDER_ID', '')
